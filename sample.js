9eda2db25fe4dac40242b7ed7e749f7b80d1dd70
khav9 <khav.saroeun@gmail.com>
2026-03-11 16:06:05 +0700

    first commit

---
+++ sample.js
@@ -0,0 +1,72 @@
+const sampleData = [
+  {
+    "title": "Cambodia Sustainable Landscape and Ecotourism Project",
+    "region": "Cambodia, East Asia and Pacific",
+    "commitment": "$51M",
+    "approvalDate": "30 May 2019",
+    "closingDate": "31 Dec 2027",
+    "link": "#",
+    "lat": 11.5564,
+    "lng": 104.9282
+  },
+  {
+    "title": "Second Test Project",
+    "region": "Vietnam, East Asia and Pacific",
+    "commitment": "$20M",
+    "approvalDate": "12 Jan 2020",
+    "closingDate": "15 Aug 2025",
+    "link": "#",
+    "lat": 13.0957,
+    "lng": 103.2022
+  },
+  {
+    "title": "Third Test Project",
+    "region": "Laos, East Asia and Pacific",
+    "commitment": "$15M",
+    "approvalDate": "05 Oct 2021",
+    "closingDate": "20 Nov 2026",
+    "link": "#",
+    "lat": 12.5657,
+    "lng": 105.0112
+  },
+  {
+    "title": "4th location",
+    "region": "Laos, East Asia and Pacific",
+    "commitment": "$15M",
+    "approvalDate": "05 Oct 2021",
+    "closingDate": "20 Nov 2026",
+    "link": "#",
+    "lat": 13.585362,
+    "lng": 102.973333
+  },
+  {
+    "title": "4.2th location",
+    "region": "Laos, East Asia and Pacific",
+    "commitment": "$15M",
+    "approvalDate": "05 Oct 2021",
+    "closingDate": "20 Nov 2026",
+    "link": "#",
+    "lat": 13.585362,
+    "lng": 102.973333
+  },
+  {
+    "title": "4.3th location",
+    "region": "Laos, East Asia and Pacific",
+    "commitment": "$15M",
+    "approvalDate": "05 Oct 2021",
+    "closingDate": "20 Nov 2026",
+    "link": "#",
+    "lat": 13.585362,
+    "lng": 102.973333
+  },
+  {
+    "title": "4.4th location",
+    "region": "Laos, East Asia and Pacific",
+    "commitment": "$15M",
+    "approvalDate": "05 Oct 2021",
+    "closingDate": "20 Nov 2026",
+    "link": "#",
+    "lat": 13.585362,
+    "lng": 102.973333
+  },
+];
